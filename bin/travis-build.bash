5f578650a5e514ab90317b1870c03d7d14a500b3
Francisco de la Vega <fdelavega@ficodes.com>
2019-03-29 12:33:58 +0100

    Update Keyrock ports

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -74,10 +74,10 @@ if [ "$INTEGRATION_TEST" = "true" ]; then
     export REQUESTS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
     docker network create main
     docker run -d --network main -e MYSQL_ROOT_PASSWORD=idm -e MYSQL_ROOT_HOST=% --name mysql mysql/mysql-server:5.7.21
-    docker run -d -p 443:443 --network main -e DATABASE_HOST=mysql -v "${TRAVIS_BUILD_DIR}/ci/idm-config.js:/opt/fiware-idm/config.js:ro" -v /etc/ssl/self_signed.key:/opt/fiware-idm/certs/self_signed.key:ro -v /usr/local/share/ca-certificates/self_signed.crt:/opt/fiware-idm/certs/self_signed.crt:ro --name idm fiware/idm
+    docker run -d -p 3000:3000 --network main -e DATABASE_HOST=mysql -v "${TRAVIS_BUILD_DIR}/ci/idm-config.js:/opt/fiware-idm/config.js:ro" -v /etc/ssl/self_signed.key:/opt/fiware-idm/certs/self_signed.key:ro -v /usr/local/share/ca-certificates/self_signed.crt:/opt/fiware-idm/certs/self_signed.crt:ro --name idm fiware/idm
 
     # Wait until idm is ready
-    test_connection 'KeyRock' https://localhost:443
+    test_connection 'KeyRock' http://localhost:3000
 fi
 
 echo "travis-build.bash is done."
